24298d43569aa941dac4a0b1b6cc125b25ad7c56
Ron Kurr <kurr@kurron.org>
2017-02-10 14:48:21 -0500

    The stacks launch but do not complete yet

--- scripts/create-stack.sh
+++ scripts/create-stack.sh
@@ -2,7 +2,7 @@
 
 # creates a stack in AWS via CloudFromation
 
-STACKNAME=${1:-Weapon-X}
+STACKNAME=${1:-Full-Stack}
 PROJECTNAME=${2:-Weapon-X}
 ENVIRONMENT=${3:-development}
 CREATOR=${4:-CloudFormation}
@@ -12,6 +12,6 @@ VALIDATE="aws cloudformation validate-template --template-body $TEMPLATELOCATION
 echo $VALIDATE
 $VALIDATE
 
-CREATE="aws cloudformation create-stack --stack-name $STACKNAME --template-body $TEMPLATELOCATION --tags Key=Project,Value=$PROJECTNAME Key=Environment,Value=$ENVIRONMENT Key=Creator,Value=$CREATOR"
+CREATE="aws cloudformation create-stack --stack-name $STACKNAME --template-body $TEMPLATELOCATION --capabilities CAPABILITY_NAMED_IAM --tags Key=Project,Value=$PROJECTNAME Key=Environment,Value=$ENVIRONMENT Key=Creator,Value=$CREATOR"
 echo $CREATE
 $CREATE
